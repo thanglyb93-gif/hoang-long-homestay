6ed57a27e84775fea57e6b3d8039d3a7b334e83e
Thang Ly <thangly93@gmail.com>
2026-05-30 15:39:26 +0700

    Allow Cloudinary images in Next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -8,5 +8,13 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
+  },
 };
 export default withNextIntl(nextConfig);
